8d52150cbf3e6353a4d9eb4afe72038c9d88697c
Dome2207 <86170885+Dome2207@users.noreply.github.com>
2021-08-04 08:54:33 -0500

    Update OPERACIONES_DE_CADENAS.py

--- OPERACIONES_DE_CADENAS.py
+++ OPERACIONES_DE_CADENAS.py
@@ -1,50 +1,50 @@
-clase  Cadena ():
-    def  __init__ ( self , cadena ):
-        yo . cadena = cadena   
-    def   recorrerCadena ( yo ):
-        imprimir ( '_______________________________________' )
-        print ( "Recorrer y presentar los datos de una cadena" )
-        imprimir ( '____________________________________' )
-        para  x  en  uno mismo . cadena :
-            imprimir ( x , '' , end = '' )  
-    def   buscarCaracter ( self , buscado ):
-        imprimir ( '______________________________' )
-        print ( "Buscar un carácter en una cadena" )
-        imprimir ( '____________________________' )
-        acum = 0
-        para  x , i  en  enumerate ( self . cadena ):
-            si  i ==  buscado :
-                acum = acum + 1
-        print ( "Su caracter se encuentra {} veces, dentro de la cadena" . formato ( acum ))
-        imprimir ()  
-    def   listaPosiciones ( self , caracter ):
-        imprimir ( '_________________________________________________________________' )
-        print ( "Retornar una lista con las posiciones dado un carácter de la cadena" )
-        imprimir ( '__________________________________________________________________' )
-        acum = 0
-        aux = []
-        para  x , i  en  enumerate ( self . cadena ):
-            acum = acum + 1
-            si  i  ==  caracter :
-                aux . añadir ( acum )
-                lista = aux
-        imprimir ( lista )        
+class Cadena():
+    def __init__(self,cadena):
+        self.cadena=cadena   
+    def  recorrerCadena(self):
+        print('_______________________________________')
+        print("Recorrer y presentar los datos de una cadena")
+        print('____________________________________')
+        for x in self.cadena:
+            print(x,'',end='')  
+    def  buscarCaracter(self,buscado):
+        print('______________________________')
+        print("Buscar un carácter en una cadena")
+        print('____________________________')
+        acum=0
+        for x,i in enumerate(self.cadena):
+            if i== buscado:
+                acum=acum+1
+        print("Su caracter se encuentra {} veces, dentro de la cadena".format(acum))
+        print()  
+    def  listaPosiciones(self,caracter):
+        print('_________________________________________________________________')
+        print("Retornar una lista con la posiciones dado un carácter de la cadena")
+        print('__________________________________________________________________')
+        acum=0
+        aux=[]
+        for x,i in enumerate(self.cadena):
+            acum=acum+1
+            if i == caracter:
+                aux.append(acum)
+                lista=aux
+        print(lista)        
                   
-    def  listaPalabras ( yo ):
-        pasar  
-    def  cadenaLista ( yo ):
-        pasar  
-    def  insertarDato ( self , buscado , posicion ):
-        pasar  
-    def  eliminarOcurrencias ( buscado ):
-        pasar  
-    def  retornaValor ( posicion ):
-        pasar  
-    def  concatenarCadena ( dato ):
-        pasar         
+    def listaPalabras(self):
+        pass  
+    def cadenaLista(self):
+        pass  
+    def insertarDato(self,buscado,posicion):
+        pass  
+    def eliminarOcurrencias(buscado):
+        pass  
+    def retornaValor(posicion):
+        pass  
+    def concatenarCadena(dato):
+        pass         
 
-cadena = 'hola como estas'
-cad =  Cadena ( cadena )
-# cad.recorrerCadena ()
-# cad.buscarCaracter ('b')
-cad . listaPosiciones ( 'v' )
+cadena='hola como estas'
+cad= Cadena(cadena)
+# cad.recorrerCadena()
+#cad.buscarCaracter('b')
+cad.listaPosiciones('v')
